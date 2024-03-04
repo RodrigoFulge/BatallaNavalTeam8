d8bc453c197ccf297c619679ae877e9288d3a3dc
RodrigoFulge <61611308+RodrigoFulge@users.noreply.github.com>
2024-03-03 21:45:48 -0600

    Versión inicial sin programación
    Este es el archivo sobre el cual se empezará a trabajar el código del juego. El resto tendrá que hacer su código en sus propias ramas

--- BatallaNaval.py
+++ BatallaNaval.py
@@ -1,45 +1,18 @@
-import random
-#Versión 1.0 - Creación del código que arma el tablero y pone los barcos
+#Versión 0.1 - Inicio
 
 class BatallaNaval:
     
     def __init__(self,numeroBarcos,TamañoTablero):
-        self.numeroBarcos = 5 
-        self.TamañoTablero = 5
+        self.numeroBarcos = int
+        self.TamañoTablero = int
         self.BarcosPuestos = {}
 
-    def Tablero(self): #Esta función definirá el tamaño del dablero Y los barcos puestos
-        tablero = []
-        for i in range (self.TamañoTablero): #Creando el tablero, considerando el espacio del inicio y las filas con números
-            fila = []
-            if i == 0:
-                for k in range(-1,self.TamañoTablero-1):
-                    if k > 2:
-                        fila.append(f"{k}")
-                    elif k < 0:
-                        fila.append("")
-                    else:
-                        fila.append(f"{k}")
+    def tablero(self):
+        pass
 
-            else:
-                fila.append(i-1)
-                for l in range(self.TamañoTablero-1):
-                    fila.append(" ")
-            tablero.append(fila)
+    def Barcos(self):
+        pass
 
-
-        for i in range(self.numeroBarcos): #Poner los barcos en el tablero YA creado
-            CoordX = random.randint(1,self.TamañoTablero-1)
-            CoordY = random.randint(1,self.TamañoTablero-1)
-            self.BarcosPuestos[i] =  [CoordX-1,CoordY-1] #Pone las coordenadas de los barcos en un diccionario
-            tablero[CoordX][CoordY] = "X" #Pone los Barcos como una X
-
-
-        for i in range(self.TamañoTablero):
-            print(tablero[i])
-
-        return(self.BarcosPuestos) #Retorna el diccionario con las coordenadas
-    
-Juego = BatallaNaval(4,4)
-Barcos = BatallaNaval.Tablero(Juego)
-print(Barcos) #Imprime el diccionario retornado por Tablero
+numeroBarcos = int
+TamañoTablero = int
+Juego = BatallaNaval(numeroBarcos,TamañoTablero)
