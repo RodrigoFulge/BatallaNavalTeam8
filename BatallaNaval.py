d55e2bfe4b1215d5d5eb50a5b776693d8df26f3f
SamuelMTZ24 <162143910+SamuelMTZ24@users.noreply.github.com>
2024-03-05 11:20:32 -0600

    Version_7
    Se hace removido de codigo para probar funciones y comprobaciones para evitar introducir casillas mas allá de las existentes

--- BatallaNaval.py
+++ BatallaNaval.py
@@ -1,5 +1,7 @@
 import random, math
-#Versión 4.0 - Removido texto adicional de los comentarios.
+#Versión 7.0 - Removido el codigo que se usaba para probrar algunos elementos. Se sugirio dejarlos como comentarios
+#Agregadas comprobaciones para evitar que se introduzan más barcos que casillas disponibles, así como para que el numero de casillas
+#no pueda ser menor a 1 fila y 1 columna. Modificaciones al funcionamiento de los aciertos y comprobaciones para no atacar fuera del tablero
 BalanceIntentos = 1.5 
 
 class BatallaNaval:
@@ -30,28 +32,63 @@ class BatallaNaval:
             self.tablero.append(fila)
         return self.tablero
 
-    def SetBarcos(self):
-        for i in range(self.numeroBarcos): #Poner los barcos en el tablero YA creado
-            CoordX = random.randint(1,self.TamañoTablero)
-            CoordY = random.randint(1,self.TamañoTablero)
-            self.BarcosPuestos[i] =  [CoordX-1,CoordY-1] #Pone las coordenadas de los barcos en un diccionario. Se resta uno para no considerar los bordes como coordenadas
-            self.tablero[CoordX][CoordY] = "X" #Pone los Barcos como una X
-        return(self.BarcosPuestos) #Retorna el diccionario con las coordenadas
-    
-#Errores
-        #No hay comprobación en caso de que dos barcos caigan en la misma posición. Hacer algo en una futura versión
+    def SetBarcos(self): #Se usa la comprensión de diccionarios aquí para generar el diccionario en una forma más comprimida con menos variables
+        Reiniciar = True
+        while Reiniciar == True:  
+            Finds = 0
+            Reiniciar = False  
+            self.BarcosPuestos = {i: [random.randint(0, self.TamañoTablero - 1), random.randint(0, self.TamañoTablero - 1)] for i in range(self.numeroBarcos)}
+            #print(self.BarcosPuestos)
+            for CompararDuped in self.BarcosPuestos.values():
+                Finds = 0
+                for AnalisisDuped in self.BarcosPuestos.values():
+                    #print(f"CompararDuped: {CompararDuped[0]}")
+                    #print(f"CompararDuped: {CompararDuped[1]}")
+                    #print(f"AnalisisDuped: {AnalisisDuped[0]}")
+                    #print(f"AnalisisDuped: {AnalisisDuped[1]}")
+                    if CompararDuped[0] == AnalisisDuped[0] and CompararDuped[1] == AnalisisDuped[1]:
+                        Finds += 1
+                        #print(f"finds: {Finds}")
+                        if Finds > 1:
+                            #print("Se encontraron duplicados, volver a generar!")
+                            Reiniciar = True #Vuelve a generar una lista de coordenadas si encuentra al menos un duplicado
+                            #print(Reiniciar)
+
+        for coordenadas in self.BarcosPuestos.values(): #Aquí values sirve para poder iterar todos los valores del diccionario
+            self.tablero[coordenadas[0] + 1][coordenadas[1] + 1] = "X"
+        #print(self.BarcosPuestos)
+        return self.BarcosPuestos
     
     def Acierto_Barco(IntentoX,IntentoY,Board): #Esta función se activará cuando el jugador acierte a un barco
         Board[IntentoX][IntentoY] = "F"
         return Board
 
-    def Fallo(self):
-        pass
+
+
 Aciertos = 0
-NumShips = int(input("¿Cuántos barcos habrá?"))
+Error = True
+while Error == True: #Compreba que el usuario solo introduzca números.
+    Error = False
+    try:
+        BoardSize = int(input("¿De que tamaño será el tablero?"))
+    except: #Si introduce algo más, recibe una excepción osea un error, y manda Error como True para repetir el código.
+        print("Por favor introduce solo números!")
+        Error = True   
+Error = True #Vuelve a establecer Error como True para próximas comprobaciones  
+
+while Error == True: #Compreba que el usuario solo introduzca números.
+    Error = False
+    try:
+        NumShips = int(input("¿Cuántos barcos habrá?"))
+    except: #Si introduce algo más, recibe una excepción osea un error, y manda Error como True para repetir el código.
+        print("Por favor introduce solo números!")
+        Error = True
+    if NumShips > ((BoardSize*BoardSize)):
+        print("No puede haber más barcos que casillas (El cuadrado del tamaño del tablero)")
+        Error= True
+Error = True  #Vuelve a establecer Error como True para próximas comprobaciones  
 IntentosRestantes = math.ceil(NumShips+(NumShips*BalanceIntentos)) #Define la cantidad de intentos. Será el número de barcos por 1.5. Aun así, se puede modificar libremente con la variable al inicio
 print(IntentosRestantes)
-BoardSize = int(input("¿De que tamaño será el tablero?"))
 Juego = BatallaNaval(NumShips,BoardSize) #Inicializa el juego
 Board = BatallaNaval.Tablero(Juego) #Inicia el tablero. 
 Barcos = BatallaNaval.SetBarcos(Juego) #Diccionario de las coordenadas. 
@@ -60,17 +97,36 @@ for i in range(BoardSize+1):
 print(Barcos) #Imprime el diccionario retornado por Tablero    
 while Aciertos != NumShips and IntentosRestantes != 0: #Mientras el número de aciertos sea menor al número de barcos, el juego seguirá
     Golpe = False
-    IntentoX = int(input("Ingresa la coordenada X de tu ataque: "))
+    while Error == True:
+        Error = False
+        try:
+            IntentoX = int(input("Ingresa la coordenada X de tu ataque: "))
+        except:
+            print("Por favor introduce solo números!")
+            Error = True
+        if IntentoX >= BoardSize: #Comprobación para no poder introducir numeros fuera de área
+            print("No puedes atacar más allá del área del tablero!")
+            Error = True
+    Error = True
     IntentoX += 1
-    IntentoY = int(input("Ingresa la coordenada Y de tu ataque: "))
+    while Error == True:
+        Error = False
+        try:
+            IntentoY = int(input("Ingresa la coordenada Y de tu ataque: "))
+        except:
+            print("Por favor introduce solo números!")
+            Error = True
+        if IntentoY >= BoardSize: #Comprobación para no poder introducir numeros fuera de área
+            print("No puedes atacar más allá del área del tablero!")
+            Error = True
+    Error = True
     IntentoY += 1 #En las dos variables de intentos se les suma 1 para que no tome en cuenta los bordes
+    
     for i in range(len(Barcos)): #Repetirá el ciclo según la cantidad de coordenadas de barcos disponibles
         indice = Barcos[i]
-        print(indice)
+        #print(indice)
         if Board[IntentoX][IntentoY] == "X": #Si hay un barco, setea Golpe a True para que ejecute la función de acierto más abajo
             Golpe = True
-        else:
-            print("No has acertado")
         if Board[IntentoX][IntentoY] == "F":  #Impide ganar otro punto si ya se había acertado a un barco en esa coordenada
             print("Ya habías acertado a ese barco. No tienes por que rematarlo")
 
@@ -80,16 +136,16 @@ while Aciertos != NumShips and IntentosRestantes != 0: #Mientras el número de a
         Board=BatallaNaval.Acierto_Barco(IntentoX,IntentoY,Board) #Actualiza el tablero mediante la función
     else:
         IntentosRestantes -= 1
+        print("No has acertado")
         print(f"Intentos restantes: {IntentosRestantes}")
 
     for i in range(BoardSize+1): #Imprime el tablero actualizado
             print(Board[i])
-if Aciertos == 5:
+if Aciertos == NumShips:
     print("Ganaste!! Derribaste todos esos barcos. Eres todo un criminal de guerra!! Tomar todas esas vidas debe hacerte sentir imponente!!")
 
 if IntentosRestantes == 0:
     print("No derribaste todos los barcos. Tus soldados han sacrificado sus vidas y aun así no has logrado defender su patria. Sus muertes han sido en vano, y tus tierras han sido tomadas. Felicidades")
 input("Presiona cualquier tecla para Salir")
     
-
 
